1ae29fd6b661e4c9b2042c4566456b4ab3a7ebb4
unknown <n.navarijo@gmail.com>
2019-02-05 20:00:21 -0600

    initial commit

---
+++ react-to-do/src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li> A todo will be given here </li>
+    );
+  }
+}
+
+export default ToDo;
