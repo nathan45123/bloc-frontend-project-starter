9fa14c410f66e80fd6162c4eef7e9eadc03f3d3c
unknown <n.navarijo@gmail.com>
2019-02-16 10:59:28 -0600

    checkpoint 5: done

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -3,7 +3,12 @@ import React, {Component} from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li> A todo will be given here </li>
+                <li>
+                <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+                 <span> { this.props.description }</span>
+                 <button onClick={this.props.deleteTodo}>Delete</button>
+                 </li>
+
     );
   }
 }
